31b1ba59640ea6acfc688fe11ffdc89f706502a1
DevSamuel06 <samuelribeiro.filho16@gmail.com>
2025-06-26 13:52:07 -0300

    testando outro formato para o banco

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 echo "⏳ Aguardando o MySQL iniciar..."
+timeout=30
 while ! nc -z mysql 3306; do
   sleep 1
+  timeout=$((timeout-1))
+  if [ "$timeout" -le 0 ]; then
+    echo "❌ Timeout ao esperar MySQL"
+    exit 1
+  fi
 done
 
 echo "✅ MySQL está no ar, iniciando backend..."
